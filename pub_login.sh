86abdf184d4cbcbd4d369d55bcf2a52c9b096e9a
Dato <barbaqadzedato3@gmail.com>
2020-11-26 23:49:34 +0400

    corected json

--- pub_login.sh
+++ pub_login.sh
@@ -24,4 +24,12 @@ if [ -z "${PUB_DEV_PUBLISH_EXPIRATION}" ]; then
 fi
 
 # Creat credentials.json file.
-echo  "{'accessToken':'${PUB_DEV_PUBLISH_ACCESS_TOKEN}','refreshToken':'${PUB_DEV_PUBLISH_REFRESH_TOKEN}','tokenEndpoint':'${PUB_DEV_PUBLISH_TOKEN_ENDPOINT}','scopes':['https://www.googleapis.com/auth/userinfo.email','openid'],'expiration':${PUB_DEV_PUBLISH_EXPIRATION}}" >  ~/.pub-cache/credentials.json
+cat <<EOF > ~/.pub-cache/credentials.json
+{
+  "accessToken":"${PUB_DEV_PUBLISH_ACCESS_TOKEN}",
+  "refreshToken":"${PUB_DEV_PUBLISH_REFRESH_TOKEN}",
+  "tokenEndpoint":"${PUB_DEV_PUBLISH_TOKEN_ENDPOINT}",
+  "scopes":["https://www.googleapis.com/auth/userinfo.email","openid"],
+  "expiration":${PUB_DEV_PUBLISH_EXPIRATION}
+}
+EOF
