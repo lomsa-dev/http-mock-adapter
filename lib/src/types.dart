1c644d56baf233cc21149750a145bca430d30d2b
Dato <barbaqadzedato3@gmail.com>
2020-12-06 13:42:56 +0400

    added documentation

--- lib/src/types.dart
+++ lib/src/types.dart
@@ -2,10 +2,13 @@ import 'package:dio/dio.dart';
 import 'package:http_mock_adapter/src/interfaces.dart';
 import 'package:http_mock_adapter/src/handlers/request_handler.dart';
 
+/// type for request chainers like [onGet]
 typedef AdapterRequest = RequestHandler Function(
   String route, {
   dynamic data,
   dynamic headers,
 });
 
+/// type for historys responsebody, which takes [RequestOptions] as a paramter
+/// and compares it's signature to saved requests and chooses right response
 typedef AdapterResponseBody = Responsable Function(RequestOptions options);
