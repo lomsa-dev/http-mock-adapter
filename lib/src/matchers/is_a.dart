17426ed95d031c852abd4e5fd8f9035dbf75156d
Peter Leibiger <kuhnroyal@gmail.com>
2021-05-25 13:24:15 +0200

    Add isA<T> & FormData matchers (#104)
    * Add isA<T> & FormData matchers
    
    * export `Matcher` base class
    
    * Update docs for IsAMatcher
    
    * Remove early return in matcher

---
+++ lib/src/matchers/is_a.dart
@@ -0,0 +1,13 @@
+import 'package:http_mock_adapter/src/matchers/matcher.dart';
+
+/// Subclass of [Matcher] that only returns true upon calling
+/// [matches] if provided with a instance having the same type [T].
+class IsAMatcher<T> extends Matcher {
+  const IsAMatcher();
+
+  @override
+  bool matches(dynamic actual) => actual is T;
+
+  @override
+  String toString() => 'IsAMatcher<$T>';
+}
