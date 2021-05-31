0f2595dfc7df914cb0f937896c41f5dfdf46233e
Peter Leibiger <kuhnroyal@gmail.com>
2021-05-31 18:56:53 +0200

    Add ListParam matcher
    * adds a matcher that matches dios `ListParam` by comparing value and format

---
+++ lib/src/matchers/list_param.dart
@@ -0,0 +1,26 @@
+import 'package:collection/collection.dart';
+import 'package:dio/dio.dart';
+import 'package:dio/src/parameter.dart';
+import 'package:http_mock_adapter/src/matchers/matcher.dart';
+
+/// Matches [ListParam] instance by comparing
+/// value and [ListFormat].
+class ListParamMatcher<T> extends Matcher {
+  final ListParam<T> expected;
+
+  const ListParamMatcher({required this.expected});
+
+  @override
+  bool matches(dynamic actual) {
+    return actual is ListParam<T> &&
+        ListEquality<T>().equals(
+          actual.value,
+          expected.value,
+        ) &&
+        actual.format == expected.format;
+  }
+
+  @override
+  String toString() =>
+      'ListParamMatcher { values: ${expected.value.length}, format ${expected.format} }';
+}
---
+++ test/matchers/list_param_test.dart
@@ -0,0 +1,64 @@
+import 'package:dio/dio.dart';
+import 'package:dio/src/parameter.dart';
+import 'package:http_mock_adapter/http_mock_adapter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('ListParamMatcher', () {
+    test('matches empty value correctly', () {
+      expect(
+        Matchers.listParam(ListParam<String>(
+          [],
+          ListFormat.pipes,
+        )).matches(ListParam<String>(
+          [],
+          ListFormat.pipes,
+        )),
+        true,
+      );
+      expect(
+        Matchers.listParam(ListParam<String>(
+          [],
+          ListFormat.pipes,
+        )).matches(ListParam<String>(
+          [],
+          ListFormat.csv,
+        )),
+        false,
+      );
+    });
+
+    test('matches value correctly', () {
+      expect(
+        Matchers.listParam(ListParam<String>(
+          ['foo', 'bar'],
+          ListFormat.pipes,
+        )).matches(ListParam<String>(
+          ['foo', 'bar'],
+          ListFormat.pipes,
+        )),
+        true,
+      );
+      expect(
+        Matchers.listParam(ListParam<String>(
+          ['foo', 'bar'],
+          ListFormat.pipes,
+        )).matches(ListParam<String>(
+          ['foo', 'baz'],
+          ListFormat.pipes,
+        )),
+        false,
+      );
+      expect(
+        Matchers.listParam(ListParam<String>(
+          ['foo', 'bar'],
+          ListFormat.pipes,
+        )).matches(ListParam<int>(
+          [1, 2],
+          ListFormat.pipes,
+        )),
+        false,
+      );
+    });
+  });
+}
