7c06779b61d06c5516c72ae9e477393fa2e23845
humblerookie <humblerookie@users.noreply.github.com>
2022-01-17 17:28:43 +0530

    chore(*): add documentation comments

--- lib/src/matchers/http_matcher.dart
+++ lib/src/matchers/http_matcher.dart
@@ -2,12 +2,30 @@ import 'package:dio/dio.dart';
 
 import '../../http_mock_adapter.dart';
 
+///
+/// This class serves as an abstraction for matching http requests.
+/// Consumers can provide customized matchers as per their use case.
+/// An instance of this is supplied while creating the [DioAdapter] or
+/// [DioInterceptor].
+///
+/// sample usage:
+///
+/// final adapter = DioAdapter(dio: dio, matcher: UrlRequestMatcher());
+///
+/// Note: The default matcher is [FullHttpRequestMatcher]
+///
 abstract class HttpRequestMatcher {
   const HttpRequestMatcher();
 
   bool matches(RequestOptions ongoingRequest, Request matcher);
 }
 
+///
+/// [FullHttpRequestMatcher] is the default matcher and matches the entire url
+/// signature including headers, request body, query parameters etc. If no
+/// matcher is specified in the adapter then it uses an instance of the this
+/// class.
+///
 class FullHttpRequestMatcher extends HttpRequestMatcher {
   const FullHttpRequestMatcher();
 
@@ -17,6 +35,11 @@ class FullHttpRequestMatcher extends HttpRequestMatcher {
   }
 }
 
+///
+/// [UrlRequestMatcher] allows one to match the request based on the
+/// path of the url and optionally the HTTP Method (via [matchMethod]) rather
+/// than the entire request signature.
+///
 class UrlRequestMatcher extends HttpRequestMatcher {
   final bool matchMethod;
 
@@ -24,9 +47,9 @@ class UrlRequestMatcher extends HttpRequestMatcher {
 
   @override
   bool matches(RequestOptions ongoingRequest, Request matcher) {
-    final isRouteTheSame =
+    final isTheSameRoute =
         ongoingRequest.doesRouteMatch(ongoingRequest.path, matcher.route);
-    return isRouteTheSame &&
+    return isTheSameRoute &&
         (!matchMethod || ongoingRequest.method == matcher.method?.name);
   }
 }
